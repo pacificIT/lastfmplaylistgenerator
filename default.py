ee35455f81bf62e9b8d018e80936cc2eb3e08955
baerland <baerland@a553bd37-59f6-f0e0-1a70-0448f53da7e4>
2011-02-12 12:42:32 +0000

    Fixed: Playlist clearing and various changes

--- default.py
+++ default.py
@@ -14,6 +14,9 @@ class MyPlayer( xbmc.Player ) :
 	countFoundTracks = 0
 	foundTracks = []
 	currentSeedingTrack = 0
+	firstRun = 0
+	SCRIPT_NAME = "LAST.FM Playlist Generator"
+
 	
 	__settings__ = xbmcaddon.Addon(id='script.lastfmplaylistgenerator')
 	apiPath = "http://ws.audioscrobbler.com/2.0/?api_key=71e468a84c1f40d4991ddccc46e40f1b"
@@ -21,31 +24,35 @@ class MyPlayer( xbmc.Player ) :
 	def __init__ ( self ):
 		xbmc.Player.__init__( self )
 		xbmc.PlayList(0).clear()
-		print "init MyPlayer"
+		self.firstRun = 1
+		xbmc.executebuiltin("Notification(" + self.SCRIPT_NAME+",Start by playing a song)")
+		#print "init MyPlayer"
 	
 	def onPlayBackStarted(self):
 		print "onPlayBackStarted"
 		xbmc.sleep(2000)
 		if xbmc.Player().isPlayingAudio() == True:
 			currentlyPlayingTitle = xbmc.Player().getMusicInfoTag().getTitle()
-			print currentlyPlayingTitle
+			print currentlyPlayingTitle + " started playing"
 			currentlyPlayingArtist = xbmc.Player().getMusicInfoTag().getArtist()
 			self.countFoundTracks = 0
 			#self.foundTracks = []
-			xbmc.PlayList(0).add(url= xbmc.Player().getMusicInfoTag().getURL(), index=0)
+			if (self.firstRun == 1):
+				self.firstRun = 0
+				#print "firstRun - clearing playlist"
+				xbmc.PlayList(0).clear()
+				xbmc.executebuiltin('XBMC.ActivateWindow(10500)')
+				xbmc.PlayList(0).add(url= xbmc.Player().getMusicInfoTag().getURL())
 			self.foundTracks += [currentlyPlayingTitle + '|' + currentlyPlayingArtist]
+			#print "Start looking for similar tracks"
 			self.fetch_similarTracks(currentlyPlayingTitle,currentlyPlayingArtist)
 	
 	def fetch_similarTracks( self, currentlyPlayingTitle, currentlyPlayingArtist ):
-		SCRIPT_NAME = "LAST.FM Playlist generator"
-		uriPB = xbmcgui.DialogProgress()
-		uriPB.create(SCRIPT_NAME, 'Finding similar tracks')
-		uriPB.update(0, str(self.countFoundTracks) + " found so far" )   
 		apiMethod = "&method=track.getsimilar"
 
 		# The url in which to use
 		Base_URL = self.apiPath + apiMethod + "&artist=" + currentlyPlayingArtist.replace(" ","+") + "&track=" + currentlyPlayingTitle.replace(" ","+")
-		print Base_URL
+		#print Base_URL
 		WebSock = urllib.urlopen(Base_URL)  # Opens a 'Socket' to URL
 		WebHTML = WebSock.read()            # Reads Contents of URL and saves to Variable
 		WebSock.close()                     # Closes connection to url
@@ -54,13 +61,8 @@ class MyPlayer( xbmc.Player ) :
 
 		similarTracks = re.findall("<track>.+?<name>(.+?)</name>.+?<artist>.+?<name>(.+?)</name>.+?</artist>.+?</track>", WebHTML, re.DOTALL )
 		countTracks = len(similarTracks)
-		progressIncrement = 1
-		if (countTracks > 0):
-			progressIncrement = float(100 / float(countTracks))
-		percentComplete = 0
 		#print "Count: " + str(countTracks)
 		for similarTrackName, similarArtistName in similarTracks:
-			percentComplete = percentComplete + progressIncrement;
 			#print "Looking for: " + similarTrackName + " - " + similarArtistName
 			similarTrackName = similarTrackName.replace("+"," ").replace("("," ").replace(")"," ").replace("&quot","'").replace("'","''").replace("&amp;","and")
 			similarArtistName = similarArtistName.replace("+"," ").replace("("," ").replace(")"," ").replace("&quot","'").replace("'","''").replace("&amp;","and")
@@ -74,26 +76,23 @@ class MyPlayer( xbmc.Player ) :
 				artist = fields[1]
 				trackTitle = fields[0]
 				trackPath = fields[3] + fields[4]
-				#print "Found: " + trackTitle + " by: " + artist
+				print "Found: " + trackTitle + " by: " + artist
 				if (similarTrackName + '|' + similarArtistName not in self.foundTracks and artist != currentlyPlayingArtist):
 					xbmc.PlayList(0).add(trackPath)
+					xbmc.executebuiltin("Container.Refresh")
 					#xbmc.executebuiltin( "AddToPlayList(" + trackPath + ";0)")
 					self.countFoundTracks += 1
 					self.foundTracks += [similarTrackName + '|' + similarArtistName]
 			if (self.countFoundTracks >= 3):
-				percentComplete = 100
 				break
 			
-			uriPB.update(int(percentComplete), str(self.countFoundTracks) + " found so far" )   
-			
-
 		if (self.countFoundTracks == 0):
-			uriPB.update(100, 'No results found...')
 			time.sleep(3)
+			self.firstRun = 1
+			print "None found"
+			xbmc.executebuiltin("Notification(" + self.SCRIPT_NAME+",No similar tracks were found)")
 			return False
 			
-		uriPB.close()
-		
 		xbmc.executebuiltin('SetCurrentPlaylist(0)')
 		
 		
